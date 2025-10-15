6ba82ab0a1f33eba2584e96a93f3eb1a405410ed
Heath Thapa <64842660+singularity-htmagarh@users.noreply.github.com>
2025-10-15 15:29:32 -0400

    Delete client directory

--- client/src/components/examples/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import Footer from '../Footer'
-
-export default function FooterExample() {
-  return (
-    <Footer onNewsletterSignup={(email) => console.log("Newsletter signup:", email)} />
-  )
-}
--- client/src/lib/authUtils.ts
+++
@@ -1,4 +0,0 @@
-// From javascript_log_in_with_replit integration
-export function isUnauthorizedError(error: Error): boolean {
-  return /^401: .*Unauthorized/.test(error.message);
-}
